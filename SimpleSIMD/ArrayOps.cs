6344b106b06c051b005ecd76f6524bf030afbc02
Gilad Freidkin <freidkin.gilad@gmail.com>
2020-09-07 01:22:37 +0300

    version bump

--- SimpleSIMD/ArrayOps.cs
+++ SimpleSIMD/ArrayOps.cs
@@ -3,7 +3,7 @@
 namespace SimpleSimd
 {
     /// <summary>
-    /// Generic SIMD accelerated array operations.
+    /// Generic SIMD-accelerated array operations.
     /// </summary>
     /// <typeparam name="T">Array element type</typeparam>
     public static partial class ArrayOps<T> where T : unmanaged
