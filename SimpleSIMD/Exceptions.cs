f28c2884123c28702674ddd4b64a471e79bc35bf
Gilad Freidkin <freidkin.gilad@gmail.com>
2020-09-17 12:35:47 +0300

    Added documentation to Exceptions class

--- SimpleSIMD/Exceptions.cs
+++ SimpleSIMD/Exceptions.cs
@@ -3,6 +3,9 @@ using System.Runtime.CompilerServices;
 
 namespace SimpleSimd
 {
+    /// <summary>
+    /// Exception helpers. Using methods to throw exceptions helps generate better JIT assembly.
+    /// </summary>
     internal static class Exceptions
     {
         [MethodImpl(MethodImplOptions.NoInlining)]
